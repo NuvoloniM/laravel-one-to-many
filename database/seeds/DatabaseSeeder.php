3f1b56a747db3f077cf2ae3c236fb0ede5d2461b
NuvoloniM <nuvoloni.m@gmail.com>
2022-06-10 16:05:39 +0200

    .

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -13,8 +13,8 @@ class DatabaseSeeder extends Seeder
     {
         $this->call(
             [
+                CategorySeeder::class,
                 PostSeeder::class, 
-                CategorySeeder::class
             ]
         );
     }
